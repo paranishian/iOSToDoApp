2ad14c454b294749a981a33ed46275494925bda7
kiyoshi nishihara <paranishian@gmail.com>
2018-07-24 21:58:31 +0900

    feat: add, deleteができた

--- iOSToDoApp/ViewController.swift
+++ iOSToDoApp/ViewController.swift
@@ -9,17 +9,6 @@
 import UIKit
 import RealmSwift
 
-struct RealmModel {
-    
-    struct realm{
-        
-        static var realmTry  = try!Realm()
-        static var realmsSet = Task()
-        static var usersSet = RealmModel.realm.realmTry.objects(Task.self)
-        
-    }
-}
-
 // MARK: Model
 
 final class TaskList: Object {
@@ -35,35 +24,76 @@ final class TaskList: Object {
 final class Task: Object {
     dynamic var text = ""
     dynamic var completed = false
+    dynamic var order = 0
 }
 
 class ViewController: UITableViewController {
+    var items: Results<Task>!
+    var notificationToken: NotificationToken?
+    var realm: Realm!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        setupRealm()
     }
     
     func setupUI() {
         title = "My Tasks"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add))
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+    
+    func setupRealm() {
+        self.realm = try! Realm()
+        func updateList() {
+            items = realm.objects(Task.self).sorted(byKeyPath: "order")
+            tableView.reloadData()
+        }
+        updateList()
+        // Notify us when Realm changes
+        self.notificationToken = self.realm.observe { _,_ in
+            updateList()
+        }
+    }
+    
+    deinit {
+        notificationToken?.invalidate()
     }
     
     // MARK: UITableView
     
     override func tableView(_ tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
-        return RealmModel.realm.usersSet.count
+        return items.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let item = RealmModel.realm.usersSet[indexPath.row]
+        let item = items[indexPath.row]
         cell.textLabel?.text = item.text
         cell.textLabel?.alpha = item.completed ? 0.5 : 1
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        print(items)
+        print("source: %@", sourceIndexPath.row)
+        print("dest  : %@", destinationIndexPath.row)
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            try! realm.write {
+                let item = items[indexPath.row]
+                realm.delete(item)
+            }
+        }
+        if editingStyle == .none {
+            print("none!!")
+        }
+    }
+
     // MARK: Functions
     
     func add() {
@@ -75,16 +105,10 @@ class ViewController: UITableViewController {
         }
         alertController.addAction(UIAlertAction(title: "Add", style: .default) { _ in
             guard let text = alertTextField.text , !text.isEmpty else { return }
-            
-            let newTask = Task(value: ["text": text])
-            
-            let realm = try! Realm()
-            try! realm.write {
-                realm.add(newTask)
-                print("Task saved")
-            }
 
-            self.tableView.reloadData()
+            try! self.realm.write {
+                self.realm.add(Task(value: ["text": text, "order": self.realm.objects(Task.self).count + 1]))
+            }
         })
         present(alertController, animated: true, completion: nil)
     }
